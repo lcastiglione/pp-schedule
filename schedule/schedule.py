e5c3c90ed40fe1c0d77118199074c58f713a452b
lcastiglione <leonardocastiglione@hotmail.com>
2023-08-17 06:41:50 -0300

    Add random functions

--- schedule/schedule.py
+++ schedule/schedule.py
@@ -1,14 +1,18 @@
+"""Módulo con funciones para manejar fechas y horas"""
+from typing import List, Union, Optional
+from datetime import datetime, timedelta, date
 import functools
 import gc
 import itertools
+import random
 import sys
 import time as tm
 import math
 import pytz
-from datetime import datetime, timedelta, date
-from typing import List, Union, Optional
+
 
 tz_default = pytz.timezone('America/Argentina/Buenos_Aires')
+LIST_DATE_FORMATS = ['%Y-%m-%d %H:%M:%S', '%Y-%m-%d', '%Y-%m-%dT%H:%M:%S.%f', '%Y-%m-%dT%H:%M:%S']
 
 
 def timeit(_func=None, *, repeat: int = 1, number: int = 1, file=sys.stdout):
@@ -95,12 +99,12 @@ def today(tz=tz_default) -> datetime:
     return datetime.now(tz=tz)
 
 
-def is_bussines_day(actual_day: datetime = today(), holidays: dict = {}) -> bool:
+def is_bussines_day(actual_day: datetime = today(), holidays: dict = None) -> bool:
     """
     Determina si un día es laborable.
 
     Args:
-        actualDay (datetime, opcional): Día a verificar. Por defecto es el día actual.
+        actual_day (datetime, opcional): Día a verificar. Por defecto es el día actual.
         holidays (List[datetime], opcional): Lista de días festivos. Por defecto es una lista vacía.
 
     Returns:
@@ -108,7 +112,7 @@ def is_bussines_day(actual_day: datetime = today(), holidays: dict = {}) -> bool
     """
     if date.weekday(actual_day) > 4:
         return False
-    if actual_day.year in holidays:
+    if holidays and actual_day.year in holidays:
         return actual_day.date() not in holidays[actual_day.year]
     return True
 
@@ -154,26 +158,26 @@ def get_seconds_by_time(value: datetime) -> int:
     return value.hour * 60 * 60 + value.minute * 60 + value.second
 
 
-def convert_string_to_date(stringDate: Union[str, List[str]]) -> Union[datetime, List[datetime], None]:
+def convert_string_to_date(date_str: Union[str, List[str]]) -> Union[datetime, List[datetime], None]:
     """
     Convierte una cadena o lista de cadenas en formato 'yyyy-mm-dd' o 'yyyy-mm-dd HH:MM:SS' a una fecha o lista de fechas datetime.
 
     Args:
-        stringDate (Union[str, List[str]]): Cadena o lista de cadenas en formato 'yyyy-mm-dd' o 'yyyy-mm-dd HH:MM:SS'.
+        date_str (Union[str, List[str]]): Cadena o lista de cadenas en formato 'yyyy-mm-dd' o 'yyyy-mm-dd HH:MM:SS'.
 
     Returns:
         Union[datetime, List[datetime], None]: Fecha o lista de fechas datetime correspondiente(s) a la cadena o lista de cadenas dada(s),
                                                o None si la cadena no se pudo convertir a fecha.
     """
-    if isinstance(stringDate, list):
-        return [convert_string_to_date(x) for x in stringDate]
-    try:
-        return datetime.strptime(stringDate, '%Y-%m-%d %H:%M:%S')
-    except Exception:
+    if isinstance(date_str, list):
+        return [convert_string_to_date(x) for x in date_str]
+    result = None
+    for date_format in LIST_DATE_FORMATS:
         try:
-            return datetime.strptime(stringDate, '%Y-%m-%d')
-        except Exception:
-            return None
+            return datetime.strptime(date_str, date_format)
+        except ValueError:
+            pass
+    return result
 
 
 def get_last_business_day() -> datetime:
@@ -184,12 +188,12 @@ def get_last_business_day() -> datetime:
         datetime: Última fecha laborable (hasta el día actual).
     """
     # Agregar a futuro los feriados nacionales
-    lastBusinessDay = today()
-    numberDay = date.weekday(lastBusinessDay)
+    last_business_day = today()
+    numberDay = date.weekday(last_business_day)
     if numberDay > 4:  # Si es sábado y domingo
         # entonces devuelvo el día viernes
-        lastBusinessDay -= timedelta(days=numberDay - 4)
-    return datetime.combine(lastBusinessDay, datetime.min.time())
+        last_business_day -= timedelta(days=numberDay - 4)
+    return datetime.combine(last_business_day, datetime.min.time())
 
 
 def get_prev_business_day(value: datetime, keep: bool = False) -> datetime:
@@ -206,10 +210,10 @@ def get_prev_business_day(value: datetime, keep: bool = False) -> datetime:
     if keep:
         if is_bussines_day(value):
             return value
-    newValue = value + timedelta(days=-1)
-    while not is_bussines_day(newValue):
-        newValue += timedelta(days=-1)
-    return newValue
+    new_value = value + timedelta(days=-1)
+    while not is_bussines_day(new_value):
+        new_value += timedelta(days=-1)
+    return new_value
 
 
 def get_next_business_day(value: datetime, keep: bool = False) -> datetime:
@@ -226,10 +230,10 @@ def get_next_business_day(value: datetime, keep: bool = False) -> datetime:
     if keep:
         if is_bussines_day(value):
             return value
-    newValue = value + timedelta(days=1)
-    while not is_bussines_day(newValue):
-        newValue += timedelta(days=1)
-    return newValue
+    new_value = value + timedelta(days=1)
+    while not is_bussines_day(new_value):
+        new_value += timedelta(days=1)
+    return new_value
 
 
 def get_business_days_between(start: datetime, end: datetime) -> List[datetime]:
@@ -243,14 +247,14 @@ def get_business_days_between(start: datetime, end: datetime) -> List[datetime]:
     Returns:
         List[datetime]: Lista de las fechas laborables en el intervalo dado.
     """
-    arrayDates = []
+    array_dates = []
     aux = start + timedelta(days=1)
     while (aux <= end):
-        numberDay = date.weekday(aux)
-        if numberDay <= 4:
-            arrayDates.append(datetime.combine(aux, datetime.min.time()))
+        number_day = date.weekday(aux)
+        if number_day <= 4:
+            array_dates.append(datetime.combine(aux, datetime.min.time()))
         aux += timedelta(days=1)
-    return arrayDates
+    return array_dates
 
 
 def up_date(ref: datetime, days: int = 0, months: int = 0, years: int = 0) -> datetime:
@@ -269,13 +273,13 @@ def up_date(ref: datetime, days: int = 0, months: int = 0, years: int = 0) -> da
     if not months and not years and not days:
         return ref
     year, month, day = ref.timetuple()[:3]
-    newMonth = month + months
-    newYear = year + years
+    new_month = month + months
+    new_year = year + years
     try:
-        return datetime(newYear + math.floor((newMonth - 1) / 12), (newMonth % 12) or 12, day) + timedelta(days=days)
+        return datetime(new_year + math.floor((new_month - 1) / 12), (new_month % 12) or 12, day) + timedelta(days=days)
     except ValueError as e:
         if str(e) == 'day is out of range for month':
-            return datetime(newYear + math.floor((newMonth - 1) / 12), (newMonth + 1 % 12) or 12, 1) + timedelta(days=days)
+            return datetime(new_year + math.floor((new_month - 1) / 12), (new_month + 1 % 12) or 12, 1) + timedelta(days=days)
         raise (e)
 
 
@@ -298,6 +302,43 @@ def add_time(ref: datetime.time, hours: Optional[int] = None, minutes: Optional[
     return (datetime.combine(today(), ref) + timedelta(hours=hours, minutes=minutes, seconds=seconds)).time()
 
 
+def convert_seconds_to_time(seconds):
+    """Convierte una cantidad de segundos a formato de tiempo (HH:MM:SS).
+
+    Args:
+        seconds (int): Cantidad de segundos.
+
+    Returns:
+        str: Cantidad de segundos convertida a formato de tiempo (HH:MM:SS).
+    """
+    hour = seconds / 60 / 60
+    minutes = (hour - math.trunc(hour)) * 60
+    sec = (min - math.trunc(min)) * 60
+    return f"{math.trunc(hour)}:{math.trunc(minutes)}:{math.trunc(sec)}"
+
+
+def ajust_date(date_str: str, days: int) -> str:
+    """
+    Ajusta una fecha en formato string sumando o restando días.
+
+    Parámetros:
+    - date_str (str): fecha en formato 'YYYY-MM-DDTHH:MM:SS'
+    - days (int): número de días a sumar o restar
+
+    Devuelve:
+    - str: fecha ajustada en formato 'YYYY-MM-DDTHH:MM:SS'
+    """
+
+    # Convertir el string a un objeto datetime
+    date_obj = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S')
+
+    # Ajustar la fecha
+    adjusted_date = date_obj + timedelta(days=days)
+
+    # Convertir el objeto datetime de nuevo a string
+    return adjusted_date.strftime('%Y-%m-%dT%H:%M:%S')
+
+
 def get_min_between_times(start: datetime.time, end: datetime.time) -> float:
     """
     Obtiene la cantidad de minutos que hay entre dos horas dadas.
@@ -309,5 +350,71 @@ def get_min_between_times(start: datetime.time, end: datetime.time) -> float:
     Returns:
         float: Cantidad de minutos que hay entre las dos horas dadas.
     """
-    r = datetime.combine(date.min, end) - datetime.combine(date.min, start)
-    return r.total_seconds() / 60 / 5
+    result = datetime.combine(date.min, end) - datetime.combine(date.min, start)
+    return result.total_seconds() / 60 / 5
+
+
+def get_random_date(origin_date=datetime.now(), days=None):
+    """Genera una fecha aleatoria en formato de string.
+
+    Args:
+        origin_date (datetime): Fecha de inicio para generar las fechas aleatorias. Por default es la fecha actual.
+        days (int): Permite sumar una suma fija de días para que no sea aleatoria la fecha generada.
+
+    Returns:
+        str: Fecha y hora aleatoria en formato 'YYYY-MM-DD HH:MM:SS'.
+    """
+    random_days = random.randint(-365, 0) if not days else days
+    random_date = origin_date + timedelta(days=random_days)
+
+    # Devuelve la fecha en el formato '%Y-%m-%d %H:%M:%S'
+    return f"{random_date.isoformat().split('T')[0]}T00:00:00"
+
+
+def get_random_date_with_time(truncate=True):
+    """Genera una fecha y hora aleatoria en formato de string. Opcionalmente, se
+    pueden truncar los último 3 dígitos de los milisegundos.
+
+    Args:
+        truncate (boolean): Define si se truncan los dígitos de los milisegundos.
+        Por default está en True.
+
+    Returns:
+        str: Fecha y hora aleatoria en formato 'YYYY-MM-DD HH:MM:SS'.
+    """
+    random_number_of_days = random.randint(-365, 0)
+    random_date = datetime.now() + timedelta(days=random_number_of_days)
+
+    # Devuelve la fecha en el formato '%Y-%m-%d %H:%M:%S:%f'
+    full_time = random_date.isoformat().split('.')
+    if truncate:
+        time = full_time[1][:3]
+        return f"{full_time[0]}.{time}000"
+    return full_time
+
+
+def get_random_time_milli():
+    """Genera un valor entero aleatorio que representa el tiempo en milisegundos.
+
+    Returns:
+        int: Valor entero aleatorio que representa el tiempo en milisegundos.
+    """
+    return random.randint(0, (86400000 - 1))
+
+
+def get_random_full_date(day=1, month=1, year=2019):
+    """Genera una fecha y hora completa aleatoria en formato de cadena.
+
+    Args:
+        day (int, optional): Día de la fecha. Valor predeterminado es 1.
+        month (int, optional): Mes de la fecha. Valor predeterminado es 1.
+        year (int, optional): Año de la fecha. Valor predeterminado es 2019.
+
+    Returns:
+        str: Fecha y hora aleatoria completa en formato 'YYYY-MM-DD HH:MM:SS.ssssss'.
+    """
+    hour = random.randint(0, 23)
+    minutes = random.randint(0, 59)
+    sec = random.randint(0, 59)
+    milli = random.randint(0, 999999)
+    return f"{year}-{month}-{day} {hour}:{minutes}:{sec}.{milli}"
